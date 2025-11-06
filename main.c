865c74e4cfa92b06030260b3d4aecd46dc7a3548
shopia1122 <shopia123@sookmyung.ac.kr>
2025-11-06 15:43:29 +0900

    main5

--- main.c
+++ main.c
@@ -3,10 +3,24 @@
 
 int main(int argc, char *argv[])
 {
-   char str[30] = "happy C programming";
-   
-   printf("문자열 \"%s\"의 길이: %i\n", str, strlen(str));
-   
+    
+  char input[100];
+  FILE* fp;
+  int i;
+    
+    
+  //fopen
+  fp = fopen("sample.txt", "w");
+
+  for(i=0;i<3;i++) {
+  //fprintf
+  printf("input a wore: ");
+  scanf("%s", input);
+  fprintf(fp, "%s\n", input);
+  
+}
+  //fclose
+   fclose(fp);
   
   system("PAUSE");	
   return 0;
